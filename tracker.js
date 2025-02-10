c52a35168474f040925afc6e09db64dd4b7e51fa
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2025-02-10 23:47:20 +0200

    Update tracker script

--- tracker.js
+++ tracker.js
@@ -1,38 +1,19 @@
-// JavaScript tracking code
+async function tracker(event='page_view', buttonsIds=[]) {
+    //const api = "https://your-server-domain.com/api.php";
+    const api = 'https://' + window.location.host + '/api.php';
 
-async function track(event='page_view', buttonsIds=[]) {
-    let url = encodeURIComponent(window.location.href);
-    let sessionId = sessionStorage.getItem("sessionId") ?? '';
-    let api = 'https://' + window.location.host + '/api.php';
-
-    if ((event==="button_click") && (buttonsIds.length > 0)) {
-        document.getElementById("myButton").addEventListener("click", async function (url, sessionId) {
-            // Send data to the server using fetch API
-            await fetch(api, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded' // Important for PHP to parse
-                },
-                body: 'event=button_click&url=' + url + '&session=' + sessionId,
-            })
-                .then(response => response.text()) // Optional: Handle server response
-                .then(data => {
-                    console.log('Tracking data sent:', data); // Log the response
-                })
-                .catch(error => {
-                    console.error('Error sending tracking data:', error);
-                });
-        });
-    }
+    //const url = encodeURIComponent(window.location.href);
+    const url = window.location.href;
+    /*const referrer = encodeURIComponent(document.referrer);
+    const userAgent = encodeURIComponent(navigator.userAgent);*/
 
     if (event==='page_view') {
         await fetch(api, {
             method: 'POST',
             headers: {
-                //'Content-Type': 'application/x-www-form-urlencoded',
                 "Content-Type": "application/json",
             },
-            body: 'event=page_view&page=' + url + '&session=' + sessionId,
+            body: JSON.stringify({ event, url })
         })
             .then(response => response.text())
             .then(data => {
@@ -43,7 +24,31 @@ async function track(event='page_view', buttonsIds=[]) {
             });
     }
 
+    // You can add more tracking options like:
+    if ((event==="button_click") && (buttonsIds.length > 0)) {
+        buttonsIds.forEach((buttonId) => {
+            document.getElementById(buttonId).addEventListener("click",
+                async function (event, url) {
+                    await fetch(api, {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ event, url })
+                    })
+                        /*.then(response => response.text())
+                        .then(data => {
+                            console.log('Tracking data sent:', data);
+                        })
+                        .catch(error => {
+                            console.error('Error sending tracking data:', error);
+                        });*/
+            });
+        });
+    }
+
     return null;
 }
 
-track();
+// Run tracker.
+tracker();
